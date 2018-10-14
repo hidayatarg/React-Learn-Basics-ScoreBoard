09d28d7c246e98b422e244a71d858da95287eed9
Hidayat Arghandabi <hidayatarg@hotmail.com>
2018-10-14 15:26:18 +0300

    embadded Javascript expression in JSX

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 
-const title = <h1>My First React Element!</h1>;
+const title = 'My First React Element!';
 
-const desc = <p> I just learned how to create a React node and render it into the DOM. </p>;
+const desc = 'I just learned how to create a React node and render it into the DOM.';
 
-const header = React.createElement(
-    'header',
-    null,
-    title,
-    desc
+const myTitleID='main-title';
+
+const header = (
+    <header>
+        <h1 id={myTitleID}>{ title }</h1>
+        <p>{ desc }</p>
+    </header>
 );
 
 ReactDOM.render(
